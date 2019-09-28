4151a2e43484255b7177b6d0f744c7623e447286
bahar <bagheri_bhr@yahoo.com>
2019-09-28 19:45:03 +0330

    modifying Product2 and Productitem

--- src/components/products2/Products2item.jsx
+++ src/components/products2/Products2item.jsx
@@ -7,7 +7,7 @@ export default class Products2item extends Component {
     }
     render() {
         return (
-            <div className="col-12 col-md-4  d-flex flex-column ">
+            <div className="col-12 col-md-4  d-flex flex-column px-1">
                 <div className="productitem">
                     <div className="d-flex justify-content-center align-items-center">
                         <h2 className="m-0 pl-3 text-white ">{this.props.name}</h2>
@@ -15,14 +15,14 @@ export default class Products2item extends Component {
                             <img src={this.props.icon} alt="" className="pl-2 w-100"/>
                         </div>
                     </div>
-                    <div className="bg-white px-2">
-                        <h3 className="pt-3">{this.props.title}</h3>
+                    <div className="bg-white px-3 pb-3">
+                        <h3 className="pt-3 font-weight-bold">{this.props.title}</h3>
                         <p className="text-right">{this.props.content}</p>
-                        <div className="d-flex justify-content-between">
+                        <div className="d-flex justify-content-between w-100">
                             <div className="like">
                                 <img src={this.props.likeicon} alt="" className="w-100"/>
                             </div>
-                            <h4>{this.props.cost}تومان</h4>
+                            <h4 className="font-weight-bold">{this.props.cost} تومان</h4>
                         </div>
                     </div>
                 </div>
