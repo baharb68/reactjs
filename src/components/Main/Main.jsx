7ba2015bfbe63788a5cc8b1acf7d8b8f46ff7fc1
bahar <bagheri_bhr@yahoo.com>
2019-09-29 08:40:41 +0330

    modifying aboutus.css for the right side of the aboutus component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './Main.css'
+import logo1 from '../img/logo.png'
 import Services from '../services/Services'
 import Orderp from  '../OrderProcess/OrderP'
 import Category from '../category/Category'
 import Products2 from '../products2/Products2'
 import Comment from '../comment/Comment'
 import Article from '../article/Article'
+import Aboutus from '../aboutus/Aboutus'
 
 export default class Main extends Component {
     render() {
@@ -26,6 +28,9 @@ export default class Main extends Component {
                 </div>
                 <div className="container-fluid article-bg py-5">
                     <Article />
+                </div>
+                <div className="container-fluid bg-gray3">
+                    <Aboutus logo={logo1} webname="VizaCart" webdesc="خدمات تبادل ارزهای مجازی" briefing="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد." btntext="ارسال"/>
                 </div>
             </div>
         )
--- src/components/aboutus/Aboutus.jsx
+++ src/components/aboutus/Aboutus.jsx
@@ -1,21 +1,24 @@
 import React, { Component } from 'react'
+import './Aboutus.css'
 
 export default class Aboutus extends Component {
     render() {
         return (
-            <div className="container d-flex flex-column flex-md-row">
-                <div className="col-12 col-md-8 d-flex flex-column">
+            <div className="w-75 about container d-flex flex-column flex-md-row py-5">
+                <div className="col-12 col-md-8 d-flex flex-column p-0 pl-md-4">
                     <div className="d-flex justify-content-start">
-                        <img src={this.props.logo} alt=""/>
-                        <div className="d-flex flex-column">
-                            <h1>{this.props.webname}</h1>
-                            <h2>{this.props.webdesc}</h2>
+                        <div>
+                            <img src={this.props.logo} alt="" className="w-100"/>
                         </div>
-                        <p>{this.props.briefing}</p>
+                        <div className="d-flex flex-column w-100 text-right justify-content-center pr-2">
+                            <h1 className="m-0">{this.props.webname}</h1>
+                            <h2 className="m-0">{this.props.webdesc}</h2>
+                        </div>
                     </div>
-
+                    <p className="text-justify mt-3">{this.props.briefing}</p>
                 </div>
-                <div className="col-12 col-md-4 d-flex flex-column">
+                <div className="col-12 col-md-4 d-flex flex-column p-0 pr-md-4">
+
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon3">{this.props.btntext}</span>
