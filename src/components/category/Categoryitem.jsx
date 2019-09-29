4f62c43ad4afdc6be842b4de3c1bb50004c8e4e4
bahar <bagheri_bhr@yahoo.com>
2019-09-29 14:55:54 +0330

    modifying category.css

--- src/components/category/Categoryitem.jsx
+++ src/components/category/Categoryitem.jsx
@@ -7,7 +7,7 @@ export default class Categoryitem extends Component {
     }
     render() {
         return (
-            <div className="col-12 col-md-4 d-flex justify-content-center mt-5">
+            <div className="category col-12 col-md-4 d-flex justify-content-center mt-5">
                 <div className="d-flex flex-column align-items-center bg-white b-shadow px-4 py-5 position-relative">
                     <div className="d-flex justify-content-center p-0 mb-3">
                         <div>
@@ -18,7 +18,7 @@ export default class Categoryitem extends Component {
                     <h3 className="mb-3">{this.props.title}</h3>
                     <p>{this.props.content}</p>
                 </div>
-                <button className="mr-auto text-white mt-4 position-absolute">{this.props.btntext}<img src={this.props.btnicon} alt="" className="mr-4"/></button>
+                <button className="categorybtn mr-auto text-white mt-4 position-absolute">{this.props.btntext}<img src={this.props.btnicon} alt="" className="mr-4"/></button>
             </div>
         )
     }
